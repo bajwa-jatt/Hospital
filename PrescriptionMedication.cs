e27892aff16b72f5f83c74df435a02704431e181
Bajwa-jatt <bajwa00119988@gmail.com>
2021-04-16 15:07:47 -0400

    first commit

---
+++ PrescriptionMedication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINAL_PROJECT_14_06
+{
+    class PrescriptionMedication :Medication
+    {
+        private string instruction;
+        
+        public PrescriptionMedication(string medicineName, double unitPrice, bool needPrescription, string instruction) : base(medicineName, unitPrice, needPrescription)
+        {
+            this.instruction = instruction;
+        }
+        public override string GetInfo()
+        {
+            return base.GetInfo() + "" + instruction;
+        }
+    }
+}
